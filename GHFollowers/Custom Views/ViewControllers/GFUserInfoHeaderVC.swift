e9cf62dd5247df695b207a4b2a72f24050b20353
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-04-16 11:30:46 +0200

    Add user info header vc

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,29 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Mohammad Eid on 16/04/2024.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Mohammad Eid on 16/04/2024.
+//
+
+import Foundation
